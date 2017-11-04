a0c850c0defd5f2f6db3ca4cd480b2bdbfd77d91
SandroCourses <sandroneumann@me.com>
2017-11-04 15:10:28 +0100

    Add navbar hiding

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,4 +8,26 @@ $( document ).ready(function() {
     pauseOnHover: false,
     slidesToScroll: 1
   });
+
+  var previousScrollValue = 0;
+
+  $(window).scroll(function() {
+    var newScrollValue = $(this).scrollTop();
+    console.log(newScrollValue - previousScrollValue);
+    if(newScrollValue > (previousScrollValue + 50)) {
+      var navHeight = $('nav').height();
+
+      $('nav').animate({
+        top: -navHeight
+      }, 150);
+
+      previousScrollValue = newScrollValue;
+    } else if(previousScrollValue > (newScrollValue + 50)) {
+      $('nav').animate({
+        top: 0
+      }, 150);
+
+      previousScrollValue = newScrollValue;
+    }
+  });
 });
